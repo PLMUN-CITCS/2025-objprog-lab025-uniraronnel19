1f70ec6f8d96c9bd9509e3db27f414686f5b4ee7
Ronnel Unira <uniraronnel_bsit@plmun.edu.ph>
2025-02-28 10:09:58 -0700

    Submitting OBJPROG Week 05 - Laboratory # 25

--- ItemDemo.java
+++ ItemDemo.java
@@ -1,3 +1,17 @@
+public class ItemDemo {
+    public static void main(String[] args) {
+        Item item1 = new Item("Laptop");
+        Item item2 = new Item("Smartphone");
+        Item item3 = new Item("Tablet");
+        
+        item1.displayItem();
+        item2.displayItem();
+        item3.displayItem();
+        
+        Item.displayObjectCount();
+    }
+}
+
 class Item {
     static int objectCount = 0;
     
@@ -14,19 +28,5 @@ class Item {
     
     public void displayItem() {
         System.out.println("Item: " + itemName);
-    }
-}
-
-public class ItemDemo {
-    public static void main(String[] args) {
-        Item item1 = new Item("Laptop");
-        Item item2 = new Item("Smartphone");
-        Item item3 = new Item("Tablet");
-        
-        item1.displayItem();
-        item2.displayItem();
-        item3.displayItem();
-        
-        Item.displayObjectCount();
     }
 }
